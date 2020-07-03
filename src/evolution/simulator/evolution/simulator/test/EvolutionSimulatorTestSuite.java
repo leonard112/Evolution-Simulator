e0ef81dad885cef2159d3d162c6f4f36e869e091
leonard112 <lcarcaramo@gmail.com>
2020-07-03 19:57:41 -0400

    Test suite for EvolutionSimulator class added

---
+++ src/evolution/simulator/evolution/simulator/test/EvolutionSimulatorTestSuite.java
@@ -0,0 +1,14 @@
+package evolution.simulator.evolution.simulator.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	TestEvolutionSimulatorSettersAndMutators.class
+})
+
+public class EvolutionSimulatorTestSuite {
+
+}
